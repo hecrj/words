983a14e8365f4fd3c82311ee7922562ad1f81ba1
Héctor <hector0193@gmail.com>
2013-12-27 20:01:01 +0100

    Added test Ruby script! :D

---
+++ test.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+
+COLUMNS = ["#", "estimation", "rel. error", "time (ms)"]
+COLUMN_SIZE = 12
+
+def title(title)
+  puts title.center(COLUMNS.size * COLUMN_SIZE, "=")
+end
+
+def row(*cols)
+  cols.each { |col| print col.to_s.ljust COLUMN_SIZE }
+  puts ""
+end
+
+def separator
+  puts "-" * (COLUMNS.size * COLUMN_SIZE)
+end
+
+test_num, memory = ARGV
+
+Dir["test/*.dat"].sort!.each do |test|
+  real = `sort -u #{test} | wc`.split[0].to_i
+  
+  title test + " - " + real.to_s
+
+  row *COLUMNS
+
+  test_num.to_i.times do |i|
+    seed = Random.new_seed % 2147483647
+
+    start = Time.now
+    result = `./words -M #{memory} -S #{seed} < #{test}`
+    finish = Time.now
+
+    estimation, total = result.split
+
+    row i+1, estimation, ((real - estimation.to_f) / real).abs.round(5), ((finish - start) * 1000).round(5)
+  end
+
+  separator
+end
+
